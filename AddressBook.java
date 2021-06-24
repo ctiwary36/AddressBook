554de660e80c51537fb473239898d69fe422d03a
Chandan Tiwary <ctiwary36@gmail.com>
2021-06-25 01:37:51 +0530

    Added:Ability to add a newAbility to add a new

--- AddressBook.java
+++ AddressBook.java
@@ -9,40 +9,64 @@ public class AddressBook {
             //creating object for address
             //taking input from user
             //printing adress detail
-            System.out.println("Welcome to Address Book Program....!!!");
-            Address address1 = new Address();
-            ArrayList<Address> list=new ArrayList<Address>();
+            //giving option to user to add or view address
+            System.out.println("Welcome to Address Book Program");
 
+            ArrayList<Address> list = new ArrayList<Address>();
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter your name:");
-            String s=sc.nextLine();
-            address1.setFirstname(s);
-            String fname= address1. getFirstname();
+            while (true) {
+                System.out.println("\n\n"
+                        + "Choose your option:\n"
+                        + "1. Create new Address\n"
+                        + "2. View all the existing address\n"
+                        + "3. exit\n");
+                int option = sc.nextInt();
+                sc.nextLine();
+                switch (option) {
+                    case 1:
+                        Address address1 = new Address();
+                        System.out.println("Enter your name:");
+                        String s = sc.nextLine();
+                        address1.setFirstname(s);
 
-            System.out.println("Enter your Last Name:");
-            address1.setLastname(sc.nextLine());
 
-            System.out.println("Enter your City:");
-            address1.setCity(sc.nextLine());
+                        System.out.println("Enter your Last Name:");
+                        address1.setLastname(sc.nextLine());
 
-            System.out.println("Enter your State:");
-            address1.setState(sc.nextLine());
+                        System.out.println("Enter your City:");
+                        address1.setCity(sc.nextLine());
 
-            System.out.println("Enter your email:");
-            address1.setEmail(sc.nextLine());
+                        System.out.println("Enter your State:");
+                        address1.setState(sc.nextLine());
 
-            System.out.println("Enter your Zip:");
-            address1.setZip(sc.next());
+                        System.out.println("Enter your email:");
+                        address1.setEmail(sc.nextLine());
 
-            System.out.println("Enter your PhoneNumber:");
-            address1.setPhoneNumber(sc.next());
+                        System.out.println("Enter your Zip:");
+                        address1.setZip(sc.next());
 
-        list.add(address1);
+                        System.out.println("Enter your PhoneNumber:");
+                        address1.setPhoneNumber(sc.next());
+                        list.add(address1);
+                        break;
+                    case 2:
+                        System.out.println("First_name\tLast_name\tCity\tState\tZip\tPhone_number\tEmail_id");
+                        for (Address addaddress1 : list) {
+                            System.out.println(addaddress1);
+                        }
+                        System.out.println("\n\n");
+                        break;
+                    case 3:
+                        return;
 
-            System.out.println("address detail" +list);
+                }
 
+
+                    System.out.println("address detail" + list);
+
+                }
+
+
+            }
         }
-
-
-    }
 
