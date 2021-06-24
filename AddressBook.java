c936ea889a2b6b1aa825d3d25185f32331523cf3
Chandan Tiwary <ctiwary36@gmail.com>
2021-06-25 02:22:26 +0530

    Added:UC3_Ability_to_edit_existing_contact_person_using_their_name

--- AddressBook.java
+++ AddressBook.java
@@ -19,13 +19,14 @@ public class AddressBook {
                         + "Choose your option:\n"
                         + "1. Create new Address\n"
                         + "2. View all the existing address\n"
-                        + "3. exit\n");
+                        + "3. Edit The Exisiting address\n"
+                        + "4. exit\n");
                 int option = sc.nextInt();
                 sc.nextLine();
                 switch (option) {
                     case 1:
                         Address address1 = new Address();
-                        System.out.println("Enter your name:");
+                        System.out.println("Enter your First name:");
                         String s = sc.nextLine();
                         address1.setFirstname(s);
 
@@ -50,15 +51,63 @@ public class AddressBook {
                         list.add(address1);
                         break;
                     case 2:
-                        System.out.println("First_name\tLast_name\tCity\tState\tZip\tPhone_number\tEmail_id");
+
                         for (Address addaddress1 : list) {
                             System.out.println(addaddress1);
                         }
                         System.out.println("\n\n");
                         break;
                     case 3:
-                        return;
+                        System.out.println("Edit record");
+                        System.out.println("Enter name to edit:");
+                        for(int i=0;i<list.size();i++){
+                            address1=list.get(i);
 
+                            if(sc.nextLine().equals(address1.getFirstname())) {
+                                System.out.println("\n\n"
+                                        + "Choose your option:\n"
+                                        + "1. enter 1 for firstname\n"
+                                        + "2. enter 2 for lastname \n"
+                                        + "3. enter 3 for city\n"
+                                        + "4. enter 4 for state\n"
+                                        + "5. enter 5 for email\n"
+                                        + "6. enter 6 Zip \n"
+                                        + "7. enter 7 phoneNumber \n"
+                                );
+                                int option1 = sc.nextInt();
+                                sc.nextInt();
+                                switch(option1) {
+                                    case 1:
+                                        System.out.println("Enter your new First name:");
+                                        address1.setFirstname(sc.nextLine());
+                                    case 2:
+                                        System.out.println("Enter your new Last name:");
+                                        address1.setLastname(sc.nextLine());
+                                    case 3:
+                                        System.out.println("Enter your new city:");
+                                        address1.setCity(sc.nextLine());
+                                    case 4:
+                                        System.out.println("Enter your new State:");
+                                        address1.setState(sc.nextLine());
+                                    case 5:
+                                        System.out.println("Enter your new Email:");
+                                        address1.setEmail(sc.nextLine());
+                                    case 6:
+                                            System.out.println("Enter your new zip:");
+                                            address1.setZip(sc.nextLine());
+                                    case 7:
+                                        System.out.println("Enter your new phone Number:");
+                                        address1.setPhoneNumber(sc.nextLine());
+                                }
+                            }
+                            else {
+                                System.out.println("User not found");
+                            }
+                        }
+                        break;
+
+                    case 4:
+                        return;
                 }
 
 
